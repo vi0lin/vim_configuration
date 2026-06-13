f780610d5b855c41d8aacf521f3a96e094c066a2
vi0lin <codekowski@gmail.com>
2026-06-13 11:16:03 +0200

    Installation

--- install.sh
+++ install.sh
@@ -270,7 +270,7 @@ install() {
     "win")
       manager="winget"
       installations="$manager install \"The Silver Searcher\" \"FZF\" \"RipGrep GNU\" "
-      installations_win_bash="apt-get install -y vim fzf ripgrep silversearcher-ag"
+      installations_win_bash="apt-get install -y vim curl wget fzf ripgrep silversearcher-ag"
       wget_plug_vim="curl -fLo ${plugins}plug.vim $plugvim"
       sudo="sudo"
       ;;
